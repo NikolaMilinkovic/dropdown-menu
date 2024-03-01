aeb1e5f05a01d6410d51da42bf0d2e2c4b2c27b0
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-03-01 14:32:07 +0100

    Remove excessie code

--- dropdown.js
+++ dropdown.js
@@ -1,11 +1,6 @@
 const dropdown = document.getElementById('dropdown-wrapper');
-const label = document.getElementById('dropdown-label');
 const content = document.getElementById('dropdown-content');
 
-dropdown.addEventListener('mouseenter', showDropdown);
-dropdown.addEventListener('mouseleave', showDropdown);
+dropdown.addEventListener('mouseenter', () => {content.classList.toggle('hidden');});
+dropdown.addEventListener('mouseleave', () => {content.classList.toggle('hidden');});
 
-function showDropdown(){
-    console.log('showDropdown activated');
-    content.classList.toggle('hidden');
-}
